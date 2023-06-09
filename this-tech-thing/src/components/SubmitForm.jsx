e0012d1f674923afa87604cc59165313f20fb81e
Eazyisreal <eazyisreal192@gmail.com>
2023-06-09 17:20:34 +0100

    [Chore]: Fixed 60%  Mentees form

--- this-tech-thing/src/components/SubmitForm.jsx
+++ this-tech-thing/src/components/SubmitForm.jsx
@@ -11,16 +11,15 @@ export default function SubmitForm() {
                 <div className="flex flex-col gap-8  w-full">
                     <div className="flex flex-col items-start gap-4">
                         <div className="flex gap-2">
-                            <img src="" alt="" />
-                            <h2 className="font-neue font-normal text-sm w-full items-start text-[#191919]">4 of 4</h2>
+
                         </div>
                             <div className="flex flex-col w-full items-start gap-4">
-                                <h2 className="w-full text-4xl font-neue font-normal text-[#191919] leading-[1.3] tracking-[0.005em]">
-                                    Thank you for your interest in This <br />
+                                <h2 className="w-full text-3xl md:text-4xl font-neue font-normal text-[#191919] leading-[1.3] tracking-[0.005em]">
+                                    Thank you for your interest in This 
                                      Tech Thing mentorship!
                                 </h2>
                                 <p className="font-neue font-normal text-base tracking-[0.05em] ">
-                                We will be in touch soon! In the meantime, follow us on <a className="underline text-xl font-neue " href="https://twitter.com/ttechthing">Twitter</a> and get <br />
+                                We will be in touch soon! In the meantime, follow us on <a className="underline text-xl font-neue " href="https://twitter.com/ttechthing">Twitter</a> and get
                                 ready for what we're building for you. 🔥 🔥
                                 </p>
                             </div>
